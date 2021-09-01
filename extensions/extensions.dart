dadf3b932fba91687fbc5d572bbec01725fd8de1
AlMamun-CSE <almamuncse2020@gmail.com>
2021-09-01 16:35:49 +0600

    Dart Extensions

---
+++ extensions/extensions.dart
@@ -0,0 +1,39 @@
+extension<T> on List<T> {
+  //Extension method: demo
+  List<T> pricesList() => this.map((item) => item).toList();
+
+  //Extension Method: Adding $ prefix
+  List<String> priceLabels(String symble) =>
+      this.map((item) => "$symble ${item}").toList();
+
+  //Extension Operator: Hike up the price by n
+  List<num> operator ^(int n) =>
+      this.map((item) => num.parse("${item}") * n).toList();
+
+  //ExtensionOperator: Reduce all prices by half
+  List<num> operator -() =>
+      this.map((item) => num.parse("${item}") / 2).toList();
+
+  //Extension Property: 3 printed labels for each price.
+  int get numPrintedPriceTags => length * 3;
+}
+
+void main() {
+  //List of prices
+  List prices = [1, 1, 99, 4];
+  // print("Price listing:");
+  // print(prices.priceList());
+
+  print("\nPrice listing with \$ prefix");
+  print(prices.priceLabels("\$"));
+
+  print("\nPrice listing after hiking up prices 3x of the original value");
+  print(prices ^ 3);
+
+  print(
+      "\nSale Price listing: prices are reduced by half of the original value");
+  print(-prices);
+
+  print("\nNumber of total price tags");
+  print(prices.numPrintedPriceTags);
+}
